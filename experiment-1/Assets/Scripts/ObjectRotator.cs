4ee2eb18cb38cb2b26a25b1241f0fbe2fa7e83f4
Pedro Cordeiro Ferreira <pedrocf96@hotmail.com>
2022-01-02 16:17:26 -0300

    experiment-1 object rotation

---
+++ experiment-1/Assets/Scripts/ObjectRotator.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectRotator : MonoBehaviour
+{
+    public Transform RotationObject;
+    private float _sensitivity;
+    private Vector3 _mouseReference;
+    private Vector3 _mouseOffset;
+    private Vector3 _rotation;
+    private bool _isRotating;
+
+    void Start()
+    {
+        _sensitivity = 0.4f;
+        _rotation = Vector3.zero;
+    }
+
+    void Update()
+    {
+        
+            if (_isRotating)
+        {
+            // offset
+            _mouseOffset = (Input.mousePosition - _mouseReference);
+
+            // apply rotation
+            _rotation.y = -(_mouseOffset.x + _mouseOffset.y) * _sensitivity;
+
+            // rotate
+            RotationObject.transform.Rotate(_rotation);
+
+            // store mouse
+            _mouseReference = Input.mousePosition;
+        }
+    }
+
+    void OnMouseDown()
+    {
+       
+        RaycastHit hitInfo = new RaycastHit();
+        bool hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo);
+
+        if (hit)
+        {
+            if(hitInfo.transform.gameObject == this.gameObject)
+            {
+                Debug.Log("HIT!!");
+                // rotating flag
+                _isRotating = true;
+
+                // store mouse
+                _mouseReference = Input.mousePosition;
+            }
+        }
+        
+    }
+
+    void OnMouseUp()
+    {
+
+        // rotating flag
+        _isRotating = false;
+    }
+
+}
