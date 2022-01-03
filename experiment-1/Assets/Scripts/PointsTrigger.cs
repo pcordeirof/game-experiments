b5c9c0258396aa24e5611b15e240c37acb537f4d
Pedro Cordeiro Ferreira <pedrocf96@hotmail.com>
2022-01-03 10:53:15 -0300

    experiment-1 mudança na cor das luzes quando ponto

--- experiment-1/Assets/Scripts/PointsTrigger.cs
+++ experiment-1/Assets/Scripts/PointsTrigger.cs
@@ -4,8 +4,28 @@ using UnityEngine;
 
 public class PointsTrigger : MonoBehaviour
 {
+    public Light Light;
+    Color DefautColor;
+
+    void Awake()
+    {
+        DefautColor = Light.color;
+    }
+
     private void OnTriggerExit(Collider other)
     {
         Destroy(other.gameObject);
+        StartCoroutine(ChangeColorLight());
     }
+
+    public IEnumerator ChangeColorLight()
+    {
+        Light.color = new Color(31f, 180f, 245f);
+        Light.intensity = 10f;
+        Light.range = 1.05f;
+        yield return new WaitForSeconds(0.5f);
+        Light.color = DefautColor;
+        Light.intensity = 20f;
+        Light.range = 10f;
+    } 
 }
