31b515c05ef474c2e2c77519841063162c5fad2a
Pedro Cordeiro Ferreira <pedrocf96@hotmail.com>
2022-01-13 11:09:04 -0300

    dotween

--- experiment-2/Assets/Scripts/Timer.cs
+++ experiment-2/Assets/Scripts/Timer.cs
@@ -1,12 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using DG.Tweening;
 public class Timer : MonoBehaviour
 {
     public PerspectiveManager perspectiveManager;
+    public Slider slider;
+    public float Time;
     public void TimeOut()
     {
         perspectiveManager.ChangePerspective();
     }
+
+    public void Start()
+    {
+        slider.DOValue(1, Time).onComplete += TimeOut;
+    }
+
 }
