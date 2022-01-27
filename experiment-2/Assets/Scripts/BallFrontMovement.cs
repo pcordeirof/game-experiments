40fd052fa08c9c2d61f2245891594738f43c04ed
Pedro Cordeiro Ferreira <pedrocf96@hotmail.com>
2022-01-27 09:59:32 -0300

    Balanceamento

--- experiment-2/Assets/Scripts/BallFrontMovement.cs
+++ experiment-2/Assets/Scripts/BallFrontMovement.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BallFrontMovement : MonoBehaviour
 {
     public Rigidbody2D BallRigidbody;
     public Animator BallAnimator;
     public Transform BallTransform;
+
+    public float jumpForce;
 
     public bool isGrounded = true;
     // Update is called once per frame
@@ -47,6 +50,11 @@ public class BallFrontMovement : MonoBehaviour
         {
             isGrounded = true;
         }
+        else if(collision.gameObject.tag == "Final")
+        {            
+            isGrounded = false;
+            SceneManager.LoadScene("Gameplay");
+        }
         else
         {
             isGrounded = false;
@@ -55,7 +63,7 @@ public class BallFrontMovement : MonoBehaviour
 
     public void Jump()
     {
-        BallRigidbody.AddRelativeForce(new Vector2(0, 5f), ForceMode2D.Impulse);
+        BallRigidbody.AddRelativeForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
         isGrounded = false;
     }
 }
